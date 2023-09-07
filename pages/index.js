74a891a3c7d08a105659ba6329a2738b500be839
immute70 <roma62634@gmail.com>
2023-09-08 00:41:55 +0300

    Add functional

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,71 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import {useEffect} from "react";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return (
+
+    'use strict';
+
+    let secretNumber = Math.trunc(Math.random() * 20) + 1;
+    let score = 20;
+    let highScore = 0;
+
+    const displayMessage = function (message) {
+        document.querySelector('.message').textContent = message;
+    }
+
+
+    useEffect(() => {
+        document.querySelector('.check').addEventListener('click', function () {
+            const guess = Number(document.querySelector('.guess').value)
+            console.log(guess, typeof guess);
+
+            if (!guess) {
+                displayMessage('No number!')
+            } else if (guess === secretNumber) {
+                displayMessage('Correct number')
+                document.querySelector('.number').textContent = secretNumber;
+                score++;
+                document.querySelector('.score').textContent = score;
+                document.querySelector('body').style.backgroundColor = '#60b347';
+
+                document.querySelector('.number').style.width = '30rem';
+
+                if (score > highScore) {
+                    highScore = score;
+                    document.querySelector('.highscore').textContent = highScore;
+                }
+            } else if (guess !== secretNumber) {
+                if (score > 1) {
+                    displayMessage(guess > secretNumber ? 'Too high' : 'Too low');
+                    score--;
+                    document.querySelector('.score').textContent = score;
+                } else {
+                    displayMessage('You lost the game. Try again)')
+                    document.querySelector('.score').textContent = 0;
+                }
+            }
+        });
+    }, []);
+
+    useEffect(() => {
+        document.querySelector('.again').addEventListener('click', function () {
+            secretNumber = Math.trunc(Math.random() * 20) + 1;
+            score = 20;
+            document.querySelector('body').style.backgroundColor = '#222';
+            document.querySelector('.number').style.width = '15rem';
+            displayMessage('Start guessing...')
+            document.querySelector('.score').textContent = score;
+            document.querySelector('.number').textContent = '?';
+            document.querySelector('.guess').value = '';
+        });
+    }, []);
+
+
+
+    return (
       <div>
         <header>
           <h1>Guess My Number!</h1>
